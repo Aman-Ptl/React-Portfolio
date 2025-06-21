1234574489a7f8f4ef6c3e542022ccf23d24044f
Ptl-aman <Ptlaman1922@email.com>
2025-06-21 13:22:18 +0530

    footer change

--- react-portfolio/src/components/Footer/Footer.jsx
+++ react-portfolio/src/components/Footer/Footer.jsx
@@ -56,7 +56,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Tarun Kaushik. All rights reserved.
+          © 2025 Aman Patel. All rights reserved.
         </p>
       </div>
     </footer>
