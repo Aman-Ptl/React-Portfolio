8cd33129078d78e4a083b003c0697227e5165ea3
Ptl-aman <Ptlaman1922@email.com>
2025-06-24 16:05:52 +0530

    updating resume

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1dTAbIWTPKcShWwDSG12jp6mAv8HNRX3U/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1Z9RP_ZqU7tb6-J9tqLi6uUYSeTJ8g2bT/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
