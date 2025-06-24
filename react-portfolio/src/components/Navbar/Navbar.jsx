7a8285dbeebfec5382f154f94dc5e139079ee704
Ptl-aman <Ptlaman1922@email.com>
2025-06-24 16:47:02 +0530

    adding links

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
         {/* Social Icons */}
         <div className="hidden md:flex space-x-4">
           <a
-            href="https://github.com"
+            href="https://github.com/Aman-Ptl"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
@@ -78,7 +78,7 @@ const Navbar = () => {
             <FaGithub size={24} />
           </a>
           <a
-            href="https://www.linkedin.com/"
+            href="https://www.linkedin.com/in/amanpatel-jnv/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
