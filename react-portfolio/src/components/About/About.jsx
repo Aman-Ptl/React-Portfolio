0441cd9501d810a5716157178fe211aa4a294a10
Ptl-aman <Ptlaman2711@gmail.com>
2025-08-19 22:40:57 +0530

    change resume

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/drive/quota"
+            href="https://drive.google.com/file/d/1p_NKeBsTZ3YuF10fL9WZ1V6S78HHR2bR/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
